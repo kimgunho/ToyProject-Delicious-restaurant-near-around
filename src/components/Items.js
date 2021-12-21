e87ccc1f54221283d8eb2158613f0d821ebd3f51
kim <yuyukgh@naver.com>
2021-12-21 16:54:54 +0900

    fix : 배포시 데이터 중복 에러

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from 'react';
 import { collection, getDocs } from 'firebase/firestore';
-import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
+// import { Swiper, SwiperSlide } from 'swiper/react/swiper-react';
 import classNames from 'classnames/bind';
 
 import styles from './Items.module.scss';
-import 'swiper/swiper.scss';
+// import 'swiper/swiper.scss';
 
 import { database } from '../firebase';
 import { UseRestauants } from '../context/useRestaurants';
@@ -18,31 +18,59 @@ function Items() {
   const [category, setCategory] = useState([]);
 
   useEffect(async () => {
-    let getAllCategory = [];
     try {
       const querySnapshot = await getDocs(collection(database, 'restaurants'));
-      querySnapshot.forEach((doc) => {
-        setRestaurants((prev) => [doc.data(), ...prev]);
-        getAllCategory.push(doc.data().category);
-        console.log(getAllCategory);
-      });
-      const filterCategory = getAllCategory.filter(
-        (item, index) => getAllCategory.indexOf(item) === index,
+      const result = querySnapshot.docs.map((doc) => doc.data());
+      setRestaurants(result);
+      const allCategory = result.map(({ category }) => category);
+      const filterCategory = allCategory.filter(
+        (item, index) => allCategory.indexOf(item) === index,
       );
-      console.log(filterCategory);
       setCategory(filterCategory);
     } catch (error) {
       console.log(error.code);
     }
   }, []);
 
+  const testF = () => {
+    console.log(restaurants);
+  };
+
   return (
     <div className={cx('container')}>
-      <h2 className={cx('title')}>오늘은 무엇을 먹을까?</h2>
+      <h2 onClick={testF} className={cx('title')}>
+        오늘은 무엇을 먹을까?
+      </h2>
 
       <Category category={category} />
-
-      <Swiper spaceBetween={10} slidesPerView={2} className={cx('slideBox')}>
+      <ul>
+        {restaurants.map((item) => {
+          const star = '⭐️'.repeat(item.score);
+          return (
+            <li className={cx('slide')} key={item.title}>
+              <ul>
+                <li> {item.title}</li>
+                <li> {item.description}</li>
+                <li> {item.address}</li>
+                <li>{star}</li>
+                {item.tag ? (
+                  <li>
+                    {item.tag.split(' ').map((tag, index) => (
+                      <span key={index}>{tag}</span>
+                    ))}
+                  </li>
+                ) : (
+                  ''
+                )}
+              </ul>
+              <button onClick={() => setRestaurantTitle(item.title)}>
+                more
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+      {/* <Swiper spaceBetween={10} slidesPerView={2} className={cx('slideBox')}>
         {restaurants?.map((item) => {
           restaurants.sort((a, b) => b.score - a.score);
           const star = '⭐️'.repeat(item.score);
@@ -69,7 +97,7 @@ function Items() {
             </SwiperSlide>
           );
         })}
-      </Swiper>
+      </Swiper> */}
     </div>
   );
 }
